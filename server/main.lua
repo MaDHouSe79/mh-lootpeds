70dfb114d6730515c16cc83849a1724be1b67e04
MaDHouSe79 <madhouse1979@gmail.com>
2023-04-16 13:57:58 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -5,89 +5,125 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local SystemIsEnable = Config.EnableOnStart
 
+local isAEventEnable = false
+
 --PLAYER ON JOIN 
-RegisterNetEvent('qb-lootpeds:server:onjoin', function()
+RegisterNetEvent('mh-lootpeds:server:onjoin', function()
     local src = source
     if SystemIsEnable then 
-        TriggerClientEvent('qb-lootpeds:client:enable', src) 
+        TriggerClientEvent('mh-lootpeds:client:enable', src) 
     end
 end)
 
 -- ENABLE LOOT SYSTEM
-RegisterServerEvent('qb-lootpeds:server:enable', function(eventname)
+RegisterServerEvent('mh-lootpeds:server:enable', function(eventname)
     SystemIsEnable = true
-    TriggerClientEvent('qb-lootpeds:client:enable', -1, eventname)
 end)
 
 -- DISABLE LOOT SYSTEM 
-RegisterServerEvent('qb-lootpeds:server:disable', function(eventname)
+RegisterServerEvent('mh-lootpeds:server:disable', function(eventname)
     SystemIsEnable = false
-    TriggerClientEvent('qb-lootpeds:client:disable', -1, eventname)
 end)
 
+-- ENABLE EVENT MODE SYSTEM
+RegisterServerEvent('mh-lootpeds:server:enableEvent', function()
+    SystemIsEnable = true
+    isAEventEnable = true
+end)
+
+-- DISABLE EVENT MODE SYSTEM 
+RegisterServerEvent('mh-lootpeds:server:disableEvent', function()
+    isAEventEnable = false
+end)
 
 --------------------------------------------------------------------------------
 ------------------------Dont Edit This Below, It Works Fine-----------------------
 -- TAKE RANDOM LOOT FROM DEAD NPC 
-RegisterNetEvent('qb-lootpeds:server:getloot', function(entity)
+RegisterNetEvent('mh-lootpeds:server:getloot', function(entity)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
-    TriggerClientEvent('qb-lootpeds:client:deleteped', -1, entity)
+    TriggerClientEvent('mh-lootpeds:client:deleteped', -1, entity)
     
-    -- Cash Chance
-    if Config.UseCash and math.random(1,100) < Config.CashChance then
-        Player.Functions.AddMoney(Config.CashType, Config.CashReward)
-    end
-        
-    -- Basic Item
-    local item = Config.Items.Basic[math.random(1, #Config.Items.Basic)]
-    if item.name then
-        Player.Functions.AddItem(item.name, 1, false)
-        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item.name], 'add')
-    end
+    if not isAEventEnable then
+        -- Cash Chance
+        if Config.UseCash and math.random(1,100) < Config.CashChance then
+            Player.Functions.AddMoney(Config.CashType, Config.CashReward)
+        end
+            
+        -- Basic Item
+        local item = Config.Items.Basic[math.random(1, #Config.Items.Basic)]
+        if item.name then
+            Player.Functions.AddItem(item.name, 1, false)
+            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item.name], 'add')
+        end
 
-    -- Ammo Item
-    if Config.UseNormalWeaponLoot or Config.UseHeavyWeaponLoot then
+        -- Ammo Item
+        if Config.UseNormalWeaponLoot or Config.UseHeavyWeaponLoot then
+            local ammo = Config.Items.Ammo[math.random(1, #Config.Items.Ammo)]
+            if ammo.name == "pistol_ammo" or ammo.name == "snp_ammo" or ammo.name == "smg_ammo" or ammo.name == "rifle_ammo" or ammo.name == "shotgun_ammo" then
+                Player.Functions.AddItem(ammo.name, 1, false)
+                TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[ammo.name], 'add')
+            end
+        end
+
+        -- Normal Weapon Item
+        if Config.UseNormalWeaponLoot then
+            local normalWeapon = Config.Items.NormalWeapons[math.random(1, #Config.Items.NormalWeapons)]
+            if math.random(1, 100) <= 15 then -- chance to get this item
+                if normalWeapon.name then
+                    Player.Functions.AddItem(normalWeapon.name, 1, false, nil)
+                    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[normalWeapon.name], 'add')
+                end
+            end
+        end
+
+        -- Heavy Weapon Item
+        if Config.UseHeavyWeaponLoot then
+            local heavyWeapon = Config.Items.HeavyWeapons[math.random(1, #Config.Items.HeavyWeapons)]
+            if math.random(1, 100) <= 2 then -- chance to get this item
+                if heavyWeapon.name then
+                    Player.Functions.AddItem(heavyWeapon.name, 1, false, nil)
+                    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[heavyWeapon.name], 'add')
+                end
+            end
+        end
+    else
+        -- Ammo Item
         local ammo = Config.Items.Ammo[math.random(1, #Config.Items.Ammo)]
         if ammo.name == "pistol_ammo" or ammo.name == "snp_ammo" or ammo.name == "smg_ammo" or ammo.name == "rifle_ammo" or ammo.name == "shotgun_ammo" then
             Player.Functions.AddItem(ammo.name, 1, false)
-            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[ammo.name], 'add')
+            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[ammo.name], 'add', 1)
         end
-    end
 
-    -- Normal Weapon Item
-    if Config.UseNormalWeaponLoot then
+        -- Normal Weapon Item
         local normalWeapon = Config.Items.NormalWeapons[math.random(1, #Config.Items.NormalWeapons)]
         if math.random(1, 100) <= 15 then -- chance to get this item
             if normalWeapon.name then
                 Player.Functions.AddItem(normalWeapon.name, 1, false, nil)
-                TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[normalWeapon.name], 'add')
+                TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[normalWeapon.name], 'add', 1)
             end
         end
-    end
 
-    -- Heavy Weapon Item
-    if Config.UseHeavyWeaponLoot then
+        -- Heavy Weapon Item
         local heavyWeapon = Config.Items.HeavyWeapons[math.random(1, #Config.Items.HeavyWeapons)]
         if math.random(1, 100) <= 2 then -- chance to get this item
             if heavyWeapon.name then
                 Player.Functions.AddItem(heavyWeapon.name, 1, false, nil)
-                TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[heavyWeapon.name], 'add')
+                TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[heavyWeapon.name], 'add', 1)
             end
         end
+
     end
-
-
 end)
 
 -- Reset On Resource Start 
 AddEventHandler('onResourceStart', function(resource)
     if resource == GetCurrentResourceName() then
         if SystemIsEnable then
-            TriggerClientEvent('qb-lootpeds:client:enable', -1)
+            TriggerClientEvent('mh-lootpeds:client:enable', -1)
         else
-            TriggerClientEvent('qb-lootpeds:client:disable', -1)
+            TriggerClientEvent('mh-lootpeds:client:disable', -1)
         end
     end
 end)
@@ -96,7 +132,7 @@ end)
 AddEventHandler('onResourceStop', function(resource)
     if resource == GetCurrentResourceName() then
         SystemIsEnable = false
-        TriggerClientEvent('qb-lootpeds:client:disable', -1)
+        TriggerClientEvent('mh-lootpeds:client:disable', -1)
     end
 end)
 
@@ -105,10 +141,10 @@ end)
 QBCore.Commands.Add(Config.Commands.toggle, Lang:t('command.start_stop', {state = Lang:t('command.on_or_off')}), {}, false, function(source, args)
     if tostring(args[1]) == 'On' then
         SystemIsEnable = true
-        TriggerClientEvent('qb-lootpeds:client:enable', -1)
+        TriggerClientEvent('mh-lootpeds:client:enable', -1)
     end
     if tostring(args[1]) == 'Off' then
         SystemIsEnable = false
-        TriggerClientEvent('qb-lootpeds:client:disable', -1)
+        TriggerClientEvent('mh-lootpeds:client:disable', -1)
     end
 end, 'admin')
