475e5deb60bc26d6122bde25520e11bc132810e4
MaDHouSe <madhouse1979@gmail.com>
2023-12-28 10:43:39 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,16 +1,9 @@
---[[ ===================================================== ]]--
---[[          QBCore Loot Peds System by MaDHouSe          ]]--
---[[ ===================================================== ]]--
-
+--[[ ===================================================== ]] --
+--[[          QBCore Loot Peds System by MaDHouSe          ]] --
+--[[ ===================================================== ]] --
 local QBCore = exports['qb-core']:GetCoreObject()
+local PlayerData = {}
 local lootedPeds = {}
-local isBlacklisted = false
-
-local function isPlayerBlacklisted()
-    QBCore.Functions.TriggerCallback("mh-blacklisted:server:isBlacklisted", function(_isBlacklisted)
-        isBlacklisted = _isBlacklisted
-    end)
-end
 
 local function IsPadAlreadyLooted(entity)
     local looted = false
@@ -25,13 +18,8 @@ end
 local function PedHasLooted(entity)
     if IsPadAlreadyLooted(entity) then return end
     lootedPeds[#lootedPeds + 1] = {ped = entity}
-    if not isBlacklisted then 
-        TriggerServerEvent('mh-blackmarket:server:taskCountAdd', 8)
-    end
 end
 
--- ON PLAYER LOAD
-local PlayerData = {}
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     PlayerData = QBCore.Functions.GetPlayerData()
     TriggerServerEvent('mh-lootpeds:server:onjoin')
@@ -39,11 +27,13 @@ end)
 
 -- Delete Dead Ped
 RegisterNetEvent("mh-lootpeds:client:deleteped", function(entity)
-    local tmpEntity = NetToPed(entity)
-    local closedPed = QBCore.Functions.GetClosestPed(GetEntityCoords(PlayerPedId()))
-    if DoesEntityExist(tmpEntity) and not IsPedAPlayer(tmpEntity) then
-        DeletePed(tmpEntity)
-        DeleteEntity(tmpEntity)
+    if Config.DeletePedsWhenLooted then
+        local tmpEntity = NetToPed(entity)
+        local closedPed = QBCore.Functions.GetClosestPed(GetEntityCoords(PlayerPedId()))
+        if DoesEntityExist(tmpEntity) and not IsPedAPlayer(tmpEntity) then
+            DeletePed(tmpEntity)
+            DeleteEntity(tmpEntity)
+        end
     end
 end)
 
@@ -55,30 +45,28 @@ end)
 
 -- TARGET SYSTEM
 exports['qb-target']:AddTargetModel(Config.PedModels, {
-    options = {
-        {
-            type = "client",
-            event = "mh-lootpeds:client:takeloot",
-            icon = 'fas fa-skull-crossbones',
-            label = Lang:t('target.label'),
-            targeticon = 'fas fa-skull-crossbones',
-            action = function(entity)
-                if IsPedAPlayer(entity) then return false end
-                if IsPadAlreadyLooted(entity) then return false end
-                if isBlacklisted then return false end
-                TriggerEvent('mh-lootpeds:client:takeloot', entity)
-            end,
-            canInteract = function(entity, distance, data)
-                if IsPedAPlayer(entity) then return false end
-                if not IsPedAPlayer(entity) and not IsEntityDead(entity) then return false end
-                if IsPadAlreadyLooted(entity) then return false end
-                if isBlacklisted then
-                    isPlayerBlacklisted()
-                    return false
-                end
-                return true
+    options = {{
+        type = "client",
+        event = "mh-lootpeds:client:takeloot",
+        icon = 'fas fa-skull-crossbones',
+        label = Lang:t('target.label'),
+        targeticon = 'fas fa-skull-crossbones',
+        action = function(entity)
+            if IsPedAPlayer(entity) then return false end
+            if IsPadAlreadyLooted(entity) then return false end
+            if isBlacklisted then return false end
+            TriggerEvent('mh-lootpeds:client:takeloot', entity)
+        end,
+        canInteract = function(entity, distance, data)
+            if IsPedAPlayer(entity) then return false end
+            if not IsPedAPlayer(entity) and not IsEntityDead(entity) then return false end
+            if IsPadAlreadyLooted(entity) then return false end
+            if isBlacklisted then
+                isPlayerBlacklisted()
+                return false
             end
-        }
-    },
-    distance = 2.5,
+            return true
+        end
+    }},
+    distance = 2.5
 })
