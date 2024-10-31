3a30e5d474be0160773db62014c5a0e04e5c6967
MaDHouSe <madhouse1979@gmail.com>
2024-10-31 21:42:00 +0100

    Update update.lua

--- server/update.lua
+++ server/update.lua
@@ -1,24 +1,19 @@
---[[ ===================================================== ]]--
---[[          QBCore Loot Peds System by MaDHouSe          ]]--
---[[ ===================================================== ]]--
-
 local script = GetCurrentResourceName()
+local url = "https://raw.githubusercontent.com" .. "/MaDHouSe79/" .. script .. "/master/version"
+local version = GetResourceMetadata(script, "version")
 
-local function checkVersion(err, responseText, headers)
-    curVersion = LoadResourceFile(script, "version")
-    if responseText == nil then
-        print(script.." check for updates failed...")
-    else
-        if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
-            print(script.." is outdated, latest version is: ^2"..responseText.."^7, installed version: ^1"..curVersion.."^7!")
-        elseif tonumber(curVersion) > tonumber(responseText) then
-            print(script.." latest version is: ^2"..responseText.."^7, installed version: ^1"..curVersion.."^7!")
+CreateThread(function()
+    PerformHttpRequest(url, function(err, text, headers)
+        if (text ~= nil) then
+            version = string.gsub(version, "%s+", "")
+            text = string.gsub(text, "%s+", "")
+            if version == text then
+                print("^0[^4Update Check^0: ^1"..script.."^0] - [Installed Version:^2 "..version.."^0] [Github Version:^2 "..text.."^0] [Status:^2Ok^0] ")
+            else
+                print("^0[^4Update Check^0: ^1"..script.."^0] - Newer version of "..script.." found [Old:^1 "..version.."^0] [New:^2 "..text.."^0]")      
+            end
         else
-            print(script.." is up to date. (^2"..curVersion.."^7)")
+            print("[^6" .. script .. "^0] Check for script update ^1FAILED^0, unable to find the host.")
         end
-    end
-end
-
-CreateThread( function()
-    PerformHttpRequest("https://raw.githubusercontent.com/MaDHouSe79/"..script.."/master/version", checkVersion, "GET")
+    end, "GET", "", "")
 end)
