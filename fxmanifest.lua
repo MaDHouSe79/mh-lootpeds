bee2d1e7d5731b65c38322429501abbb2f5f8d3e
MaDHouSe <madhouse1979@gmail.com>
2024-11-26 05:00:37 +0100

    ..

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,6 +13,5 @@ shared_scripts {
 
 client_scripts {'client/main.lua'}
 server_scripts {'@oxmysql/lib/MySQL.lua', 'server/main.lua', 'server/update.lua'}
-dependencies {'qb-core', 'mh-core'}
 
 lua54 'yes'
